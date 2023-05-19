51eb8d4e21a915d26a3ce8c8a0c106b720121f31
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-05-19 14:23:20 +0300

    upd(start)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio'
 
+const input = document.querySelector('#search-box');
 const DEBOUNCE_DELAY = 300;
+function dataHandling(event){
+    const textContent = event.currentTarget.trim();
+    if(textContent === '' ){
+        clearHTML();
+        return;
+    }
+}
+function fetchCountries(i) {
+    fetch("https://restcountries.com/v3.1/all?fields=name,flags.svg,name.official,capital,population,languages")
+        .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+        })
+      .then(data => {
+        let x = [];
+        let i = JSON.parse(data).map(Object.values);
+        if (){
+
+        }
+
+    })
+
+        .catch(error=>{
+            console.log(error)
+        })
+}
